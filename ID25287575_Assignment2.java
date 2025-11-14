f721f79e4dd1714db124125e1e6e40fc459bfd33
LvLTroubleshooter <ismailliam144@gmail.com>
2025-11-14 09:49:33 +0000

    empty template

---
+++ ID25287575_Assignment2.java
@@ -0,0 +1,15 @@
+/*
+ * ID25287575_Assignment2.java
+ * Student ID: 25287575
+ * Name: Ismail BAHLAOUI
+ *
+ * Forest-fire simulation with data parallelism and task parallelism.
+ */
+
+public class ID25287575_Assignment2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("lets get started");
+    }
+}
